99f804601eef2f4a220b3010a0f718803fd05fb7
Nexpreet <lukabilanovic33@gmail.com>
2024-10-26 20:36:17 +0200

    Bence Barens

--- option/option.h
+++ option/option.h
@@ -28,33 +28,3 @@ void Izabrana_Opcija(Opcija_Enum Opcija);
 
 
 #endif
-#ifndef OPTION_H
-#define OPTION_H
-
-
-typedef enum {
-
-    /*
-    Manipulisanje imenima
-    */
-    DELETE,
-    INSERT,
-
-    /*
-    Manipulisanje sa sortiranjem
-    */
-    SORT,
-    INVSORT,
-
-    /*
-    Pojavljivalnja
-    */
-    ALL,
-    LEFT,
-    RIGHT
-}Opcija_Enum;
-
-void Izabrana_Opcija(Opcija_Enum Opcija);
-
-
-#endif
